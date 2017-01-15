ea7fce8fc593fb92eb50bf962fe1dee5d5e9c390
linbojin <linbojin203@gmail.com>
2017-01-15 21:24:41 +0800

    add selection sorting implementation

---
+++ doc/04-ElementarySorts.md
@@ -0,0 +1,38 @@
+# Elementary Sorts
+
+## Sorting problem
+![](media/14844859206056.jpg)
+![](media/14844859330288.jpg)
+![](media/14844859443535.jpg)
+
+**Callbacks**<br>
+![](media/14844859990177.jpg)
+![](media/14844860095537.jpg)
+
+**Total order**<br>
+![](media/14844860790344.jpg)
+
+**Implementing the Comparable interface**<br>
+![](media/14844861292119.jpg)
+
+**Two useful sorting abstractions**<br>
+![](media/14844861619602.jpg)
+![](media/14844861823194.jpg)
+
+------------------------------------------------------------------
+
+## Selection sort
+![](selection-sort_c041bf.gif)
+![](media/14844862244139.jpg)
+![](media/14844862446488.jpg)
+
+**Selection sort inner loop**<br>
+![](media/14844863579685.jpg)
+
+**Java implementation**<br>
+![](media/14844863880492.jpg)
+
+**mathematical analysis**<br>
+![](media/14844864203113.jpg)
+
+
---
+++ java/src/main/java/com/linbo/algs/sortings/Selection.java
@@ -0,0 +1,64 @@
+package com.linbo.algs.sortings;
+
+import java.util.Comparator;
+
+/**
+ * Created by @linbojin on 15/1/17.
+ * This class provides static methods for sorting an
+ *  array using selection sort.
+ */
+public class Selection {
+
+  // This class should not be instantiated.
+  private Selection() { }
+
+  public static void sort(Comparable[] a) {
+    int n = a.length;
+    for (int i = 0; i < n; i++) {
+      int min = i;
+      for (int j = i+1; j < n; j++) {
+        if (less(a[j], a[min])) min = j;
+      }
+      exch(a, i, min);
+    }
+  }
+
+  public static void sort(Comparable[] a, Comparator comparator) {
+    int n = a.length;
+    for (int i = 0; i < n; i++) {
+      int min = i;
+      for (int j = i+1; j < n; j++) {
+        if (less(comparator, a[j], a[min])) min = j;
+      }
+      exch(a, i, min);
+    }
+  }
+
+  private static boolean less(Comparable v, Comparable w) {
+    return v.compareTo(w) < 0;
+  }
+
+  private static boolean less(Comparator comparator, Object v, Object w) {
+    return comparator.compare(v, w) < 0;
+  }
+
+  private static void exch(Object[] a, int i, int j) {
+    Object swap = a[i];
+    a[i] = a[j];
+    a[j] = swap;
+  }
+
+
+  public static void main(String args[]) {
+    Integer[] a = {6,2,1,4};
+
+    Selection.sort(a);
+
+    for (int i: a) {
+      System.out.print(i + " ");
+    }
+
+  }
+
+
+}
