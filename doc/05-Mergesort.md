d5ce7a17cc71c12ad2aed823edb4c441a5265533
linbojin <linbojin203@gmail.com>
2017-01-22 18:03:36 +0800

    add recursive version of merge sort

---
+++ doc/05-Mergesort.md
@@ -0,0 +1,54 @@
+# Mergesort
+
+## Two classic sorting algorithms
+![](media/14849910002972.jpg)
+
+## Mergesort
+![](media/14849910371420.jpg)
+
+**Abstract in-place merge**<br>
+![](media/14849910717982.jpg)
+
+**Merging: Java implementation**
+![](media/14849910963673.jpg)
+
+**Assertions**<br>
+![](media/14849911323857.jpg)
+
+**Mergesort: Java implementation**<br>
+![](media/14849911863278.jpg)
+
+### Trace and Animation
+![](media/14849911572250.jpg)
+
+![random](media/merge-sort_random.gif)
+![nearly-sorted](media/merge-sort_nearly-sorted.gif)<br>
+
+### Empirical analysis
+![](media/14849913219037.jpg)
+
+### Mergesort: number of compares and array accesses
+![](media/14850781475793.jpg)
+
+**Divide-and-conquer recurrence**<br>
+![](media/14850782241288.jpg)
+![](media/14850783255836.jpg)<br>
+![](media/14850783550062.jpg)<br>
+![](media/14850783669122.jpg)
+### Mergesort analysis: memory![](media/14850785620225.jpg)
+
+### Practical improvements
+![](media/14850786281865.jpg)<br>
+![](media/14850786471126.jpg)<br>
+![](media/14850787857933.jpg)
+
+### visualization
+![](media/14850789084991.jpg)
+
+-------------------------------------------------
+
+## Bottom-up mergesort
+![](media/14850791044082.jpg)
+![](media/14850791171463.jpg)
+
+
---
+++ java/src/main/java/com/linbo/algs/sortings/Merge.java
@@ -0,0 +1,53 @@
+package com.linbo.algs.sortings;
+
+import java.util.Comparator;
+
+/**
+ * Created by @linbojin on 16/1/17.
+ *  This class provides static methods for sorting an
+ *  array using mergesort.
+ */
+public class Merge {
+
+  private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+    for (int k = lo; k <= hi; k++) {
+      aux[k] = a[k];
+    }
+
+    int i = lo, j = mid + 1;
+    for (int k = lo; k <= hi; k++) {
+      if (i > mid) a[k] = aux[j++];
+      else if (j > hi) a[k] = aux[i++];
+      else if (less(aux[j], aux[i])) a[k] = aux[j++];
+      else a[k] = aux[i++];
+    }
+  }
+
+  private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+    if (hi <= lo) return;
+    int mid = (lo + hi) / 2;
+    sort(a, aux, lo, mid);
+    sort(a, aux, mid + 1, hi);
+    merge(a, aux, lo, mid, hi);
+  }
+
+  public static void sort(Comparable[] a) {
+    Comparable[] aux = new Comparable[a.length];
+    sort(a, aux, 0, a.length - 1);
+  }
+
+  private static boolean less(Comparable v, Comparable w) {
+    return v.compareTo(w) < 0;
+  }
+
+
+  public static void main(String args[]) {
+    Integer[] a = {6,11, 7, 12, 5, 4, 2,1,4};
+
+    Merge.sort(a);
+
+    for (int i: a) {
+      System.out.print(i + " ");
+    }
+  }
+}
